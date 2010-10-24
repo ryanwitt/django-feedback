952b79ea86630f777c7fbd7da7cbffcdf7cccbe3
ryanwitt <onecreativenerd@gmail.com>
2010-10-23 20:23:00 -0700

    Switch to the django-1.2 messaging framework and make message and level configurable.

--- feedback/views.py
+++ feedback/views.py
@@ -1,16 +1,17 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.contrib import messages
 
 from feedback.forms import FeedbackForm
 
-def leave_feedback(request):
+def leave_feedback(request, message='Thanks for your feedback!', message_level=messages.SUCCESS):
     form = FeedbackForm(request.POST or None)
     if form.is_valid():
         feedback = form.save(commit=False)
         if request.user.is_authenticated():
             feedback.user = request.user
-            request.user.message_set.create(message="Your feedback has been saved successfully.")
+            messages.add_message(request, message_level, message)
         feedback.save()
-        return HttpResponseRedirect(request.GET.get('next', request.META.get('HTTP_REFERER', '/')))
+        return HttpResponseRedirect(request.POST.get('next', request.META.get('HTTP_REFERER', '/')))
     return render_to_response('feedback/feedback_form.html', {'form': form}, context_instance=RequestContext(request))
